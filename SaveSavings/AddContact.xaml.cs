78cb2dd5b45552dbaa24f11ab6fb1d862f06202b
Victor Isaev <visaev@saber3d.ru>
2018-04-01 23:52:31 +0200

    First playable by example

---
+++ SaveSavings/AddContact.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SaveSavings
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddContact : Page
+    {
+        public AddContact()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AddContact_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseHelperClass Db_Helper = new DatabaseHelperClass();//Creating object for DatabaseHelperClass.cs from ViewModel/DatabaseHelperClass.cs    
+            if (NametxtBx.Text != "" & PhonetxtBx.Text != "")
+            {
+                Db_Helper.Insert(new Contacts(NametxtBx.Text, PhonetxtBx.Text));
+                Frame.Navigate(typeof(HomePage));//after add contact redirect to contact listbox page    
+            }
+            else
+            {
+                MessageDialog messageDialog = new MessageDialog("Please fill two fields");//Text should not be empty    
+                await messageDialog.ShowAsync();
+            }
+        }
+    }
+}
